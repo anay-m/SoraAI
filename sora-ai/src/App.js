7e93db68b791332132398ef3f2fb2d604fd8509d
Anay Modi <anaym@umich.edu>
2024-06-07 21:13:10 -0500

    initial frontend

---
+++ sora-ai/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import DetectFakes from './DetectFakes';
+
+function App() {
+  return (
+    <div className="App">
+      <DetectFakes />
+    </div>
+  );
+}
+
+export default App;
